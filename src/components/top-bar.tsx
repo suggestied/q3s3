81285746e71f3c3166edeef8f8fdce084b600ad4
Dev Dev <suggestied@gmail.com>
2024-09-17 17:20:24 +0200

    Better top bar

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,4 +1,5 @@
-"'use client'"
+// "'use client
+"use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
@@ -14,22 +15,31 @@ export function TopBarComponent() {
     <div className="relative">
       <div className="bg-orange-400 text-white p-4 flex items-center justify-between">
         <div className="text-2xl font-bold">Q3</div>
-        <div className="text-xl flex gap-4">
+        <div className="text-md flex gap-4">
           {pages.map((page) => (
-            <a key={page.url} href={
-              page.url
-            } className="text-white hover:text-gray-200">{page.name}</a>
-
+            <a
+              key={page.url}
+              href={page.url}
+              className={`text-white font-mono hover:text-gray-200 ${
+                page.url === location.pathname
+                  ? "underline underline-offset-4"
+                  : ""
+              }`}
+            >
+              {page.name}
+            </a>
           ))}
         </div>
         <Avatar>
-          <AvatarImage src="/placeholder.svg?height=40&width=40" alt="User avatar" />
+          <AvatarImage
+            src="/placeholder.svg?height=40&width=40"
+            alt="User avatar"
+          />
           <AvatarFallback>U</AvatarFallback>
         </Avatar>
       </div>
       {/* absolute orange h-96 */}
       <div className="absolute top-16 left-0 w-full bg-orange-400 h-64"></div>
-      
     </div>
-  )
+  );
 }
