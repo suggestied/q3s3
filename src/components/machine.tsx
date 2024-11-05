f17b859eaf02e4074cf5a495bceef4cefebaa56b
Luc Lammers <luclammers06@gmail.com>
2024-11-05 13:59:01 +0100

    Machine

--- src/components/machine.tsx
+++ src/components/machine.tsx
@@ -4,6 +4,7 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Machine, Mold } from "@/types";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Circle, CircleCheck, Clock, Plug, Unplug } from "lucide-react";
 
 
 interface MachineComponentProps {
@@ -28,25 +29,99 @@ export function MachineComponent({ machine, withMold }: MachineComponentProps) {
         if (active) {
           return "bg-green-500";
         } else {
-          return "bg-red-500";
+          return "bg-gray-500";
         }
       case "border":
         if (active) {
           return "border-green-500";
         } else {
-          return "border-red-500";
+          return "border-gray-500";
         }
       case "status":
         if (active) {
-          return "text-green-300";
+          return "bg-green-800";
         } else {
-          return "text-red-300";
+          return "bg-gray-800";
         }
     }
   };
 
   return (
-    <div className="">
+    <div className={`w-56 h-44 shadow-sm rounded-lg overflow-hidden flex justify-between text-white ${getHealthColor(!withMold?.isOffline, 'border')}`}>
+
+      <div className={`flex flex-col w-full p-5 justify-between rounded-lg ${getHealthColor(!withMold?.isOffline, 'bg')}`}>
+
+        <div className="flex gap-5 items-center">
+
+          <span className="text-3xl font-bold">
+            {machine.name}
+          </span>
+
+          <div className="flex gap-1">
+            <Clock />
+            <span>
+              24h
+            </span>
+          </div>
+
+        </div>
+
+        {/* <span className="text-xs font-medium opacity-60">
+          {withMold?.name}
+        </span> */}
+
+        <div className="flex flex-row justify-between">
+
+          <div className="flex flex-col text-center">
+
+            <span className="text-md font-bold">
+              {withMold?.shots24h}
+            </span>
+
+            <span className="text-xs">
+              Shots
+            </span>
+
+          </div>
+
+          <div className="flex flex-col text-center">
+
+            <span className="text-md font-bold">
+              {withMold?.avgShotDuration24h}s
+            </span>
+
+            <span className="text-xs">
+              Per shot
+            </span>
+
+          </div>
+
+        </div>
+
+        <div className={`w-full h-10 rounded-md flex p-5 gap-5 items-center ${getHealthColor(!withMold?.isOffline, "status")}`}>
+
+          {
+            withMold?.isOffline ?
+              <Unplug /> :
+              <CircleCheck />
+          }
+          <span>
+            {
+              withMold?.isOffline ?
+                'Inactive' :
+                'Active'
+            }
+          </span>
+
+        </div>
+
+      </div>
+
+      <div className="flex w-full justify-between items-center">
+
+        
+
+      </div>
 
     </div>
   );
