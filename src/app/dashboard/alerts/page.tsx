617b5217f6893151a51f88adb3c8e8b475c4cbd8
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2024-12-09 11:29:10 +0100

    Add header to alert page

--- src/app/dashboard/alerts/page.tsx
+++ src/app/dashboard/alerts/page.tsx
@@ -1,8 +1,11 @@
+import Header from "../header";
 import { AlertCard } from "./alert";
 
 
 export default function Page() {
     return (
+       <>
+       <Header/>
        <div className="flex flex-col space-y-4 px-2 pt-2">
         <AlertCard 
             title="Heads up!" 
@@ -10,5 +13,6 @@ export default function Page() {
             status="Inactief"
         />
        </div>
+       </>
     )
 }
